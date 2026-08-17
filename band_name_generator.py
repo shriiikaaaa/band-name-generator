f1948154dd8710a972ca60afd5491af9eed3ff0b
Shrika <shrikarana@gmail.com>
2026-08-17 23:01:08 +0530

    Add band name genrator

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("Which city are you from? ")
+pet = input("What is the name of your pet? ")
+
+print("Your band name could be: " + city + " " + pet)
